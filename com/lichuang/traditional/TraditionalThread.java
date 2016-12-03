b2ac92c43617096ae82c395ba592ef72e4fb0c3f
li-chuang <278747148@qq.com>
2016-12-03 22:54:46 +0800

    Create TraditionalThread.java

---
+++ com/lichuang/traditional/TraditionalThread.java
@@ -0,0 +1,25 @@
+package com.lichuang.traditional;
+
+
+/**
+ * 1.最基础的线程使用
+ *
+ */
+public class TraditionalThread {
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Hello World!!");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+	}
+}
