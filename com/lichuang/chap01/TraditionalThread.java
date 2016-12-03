fad9129d2613e3412464929d077bbdadc1525c21
li-chuang <278747148@qq.com>
2016-12-04 00:45:24 +0800

    Update TraditionalThread.java

--- com/lichuang/chap01/TraditionalThread.java
+++ com/lichuang/chap01/TraditionalThread.java
@@ -2,6 +2,8 @@ package com.lichuang.chap01;
 
 /**
  * 1.最基础的线程使用
+ *   这里有两个方法类Tread与Runnable，一个是线程类，一个是任务类，分工相当明确
+ *   希望在以后的工作中，有关线程的使用时也可以按这样明确的分工写代码。
  *
  */
 public class TraditionalThread {
