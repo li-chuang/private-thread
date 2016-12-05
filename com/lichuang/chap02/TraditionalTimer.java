edc9e6f2bcae1ab3751cbad3e9046e0f7e200c21
li-chuang <278747148@qq.com>
2016-12-05 23:11:19 +0800

    Update TraditionalTimer.java

--- com/lichuang/chap02/TraditionalTimer.java
+++ com/lichuang/chap02/TraditionalTimer.java
@@ -1,6 +1,9 @@
 package com.lichuang.chap02;
 
+import java.applet.AudioClip;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -10,6 +13,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
+import javax.swing.JApplet;
 
 
 /**
@@ -26,7 +30,9 @@ public class TraditionalTimer {
 		cal.set(Calendar.MINUTE, 23);		
 		executeDate(new MyTimerTask(),cal);*/
 		
-		executeDelay(new MyTimerTask(),5000);
+		//executeDelay(new MyTimerTask(),5000);
+		
+		executePeriod(new MyTimerTask(),1000);
 	}
 
 	//在预定的时间点执行任务，以后可以有定时发邮件，定时保存数据库数据等。作用很广
@@ -39,18 +45,24 @@ public class TraditionalTimer {
 		new Timer().schedule(task, delay);
 	}
 	
+	public static void executePeriod(TimerTask task,long period){
+		new Timer().schedule(task, Calendar.getInstance().getTime(), period);
+	}
+	
 }
 //任务类，自己想写多少任务都可以
 class MyTimerTask extends TimerTask{
 	@Override
 	public void run() {
 		System.out.println(Calendar.getInstance().getTime());
-		playMusic();
 		System.out.println("Booming! ");
 	}
-	
-	//Java本身只支持mid格式的音频，用JMF可以播放其他格式的音频文件
-	public void playMusic(){
+}
+
+//Java本身只支持mid格式的音频，用JMF可以播放其他格式的音频文件
+class MusicTimerTask extends TimerTask{
+	@Override
+	public void run() {
 		try{
 			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("music/255.mid"));
 		    AudioFormat aif = ais.getFormat();
@@ -72,15 +84,19 @@ class MyTimerTask extends TimerTask{
 		catch (Exception e){
 			e.printStackTrace();
 		}
-		/*//URL mus=this.getClass().getResource("/music/Keepup.wav");
+	}	
+}
+
+class SecondMusicTimerTask extends TimerTask{
+	@Override
+	public void run() {
 		URL mus=null;
 		try {
-			mus = new File("/董贞 - 两两相忘.mp3").toURI().toURL();
+			mus = new File("music/255.mid").toURI().toURL();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		AudioClip snd =JApplet.newAudioClip(mus);
-		//snd.loop();
-		snd.play();*/
-	}
+		snd.loop();		
+	}	
 }
