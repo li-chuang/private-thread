8af42e3fb523306f4d635fe055d8902646b3fed5
li-chuang <278747148@qq.com>
2016-12-19 23:31:59 +0800

    Create LockTest.java

---
+++ com/lichuang/chap08/LockTest.java
@@ -0,0 +1,61 @@
+package com.lichuang.chap08;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+//线程锁Lock，与synchronized相比更加面向对象
+//作用实际上与synchronized类似
+public class LockTest {
+
+	public static void main(String[] args) {
+		final Business business = new Business(5);
+		new Thread(new Runnable() {			
+			@Override
+			public void run() {
+				while(true){
+					business.increse();
+				}
+			}
+		}).start();
+		new Thread(new Runnable() {		
+			@Override
+			public void run() {
+				while(true){
+					business.descrese();
+				}
+			}
+		}).start();
+	}
+}
+
+class Business{
+	private int number = 0;
+	Lock lock = new ReentrantLock();
+	
+	public Business(int number){
+		this.number = number;
+	}
+	
+	public void increse(){
+		lock.lock();
+		try {
+			number++;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			lock.unlock();
+		}
+	}
+	
+	public void descrese(){
+		lock.lock();
+		try {
+			number--;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			lock.unlock();
+		}
+	}
+}
