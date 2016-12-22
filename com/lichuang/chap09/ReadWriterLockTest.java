d63382579225b9ac498dcc6ac9c12eedbfc7ef76
li-chuang <278747148@qq.com>
2016-12-22 21:35:34 +0800

    Update ReadWriterLockTest.java

--- com/lichuang/chap09/ReadWriterLockTest.java
+++ com/lichuang/chap09/ReadWriterLockTest.java
@@ -4,9 +4,11 @@ import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * 读写锁
  * 原则：线程中不存放线程控制逻辑，控制逻辑都放在资源文件中
+ * 
  *
  */
 public class ReadWriterLockTest {
@@ -40,10 +42,11 @@ public class ReadWriterLockTest {
 //此为共享数据，只能有一个线程能写该数据，但可以有多个线程同时读取该数据
 class Queue{
 	private Object data = null;
+	//此为读写锁，加读锁后可以多线程同时读，但加写锁后，只能一个线程单独写
 	ReadWriteLock rwl = new ReentrantReadWriteLock();
 	
 	public Object get(){
-		rwl.readLock().lock();
+		rwl.readLock().lock();//加读锁
 		try {
 			Thread.sleep((long)Math.random()*1000);
 		} catch (InterruptedException e) {
@@ -55,7 +58,7 @@ class Queue{
 	}
 	
 	public void put(Object data){
-		rwl.writeLock().lock();
+		rwl.writeLock().lock();//加写锁
 		try {
 			Thread.sleep((long) (Math.random()*1000));
 			this.data = data;
@@ -65,5 +68,4 @@ class Queue{
 			rwl.writeLock().unlock();
 		}
 	}
-	
 }
