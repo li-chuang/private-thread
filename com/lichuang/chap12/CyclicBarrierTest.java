60e7f1ae2f532f83f691c46b77679bb5e962e5f4
li-chuang <278747148@qq.com>
2016-12-24 23:23:07 +0800

    Update CyclicBarrierTest.java

--- com/lichuang/chap12/CyclicBarrierTest.java
+++ com/lichuang/chap12/CyclicBarrierTest.java
@@ -1,5 +1,7 @@
 package com.lichuang.chap12;
 
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,17 +20,26 @@ public class CyclicBarrierTest {
 			Runnable runnable = new Runnable() {
 				@Override
 				public void run() {
-					//int result = cb.getNumberWaiting();
-					// cb.
-					System.out.println("--------A--------");
-					business.getCount();
-					System.out.println("--------B--------");
+					String name = business.getCount();
+					try {  
+			            Thread.sleep(1000 * (new Random()).nextInt(8));  
+			            System.out.println(name + " 准备好了...");  
+			            // barrier的await方法，在所有参与者都已经在此 barrier 上调用 await 方法之前，将一直等待。  
+			            cb.await();  
+			        } catch (InterruptedException e) {  
+			            e.printStackTrace();  
+			        } catch (BrokenBarrierException e) {  
+			            e.printStackTrace();  
+			        }  
+			        System.out.println(name + " 起跑！");  
+					//business.getCount();
 				}
 			};
 			service.execute(runnable);
 		}
 		service.shutdown();
 	}
+
 }
 
 class Business {
@@ -50,8 +61,9 @@ class Business {
 		count--;
 	}
 
-	public void getCount() {
+	public String getCount() {
 		increase();
 		System.out.println(Thread.currentThread() + ",已经进入第" + count + "个并发！");
+		return count+"号";
 	}
 }
